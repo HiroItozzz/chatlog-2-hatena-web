06dda4a783b94379954f79e00d688734a0c1bb6f
Hiro Ito <mobiiii24@gmail.com>
2025-12-26 14:15:04 +0900

    add:auth ver_2

--- web/routers/auth.py
+++ web/routers/auth.py
@@ -1,10 +1,17 @@
-"""https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#use-the-form-data"""
+"""
+https://fastapi.tiangolo.com/tutorial/security/simple-oauth2/#use-the-form-data
+https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/#install-pwdlib
+"""
 
 import logging
+from datetime import datetime, timedelta, timezone
 from typing import Annotated
 
+import jwt
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from pwdlib import PasswordHash
 from pydantic import BaseModel, ValidationError
 
 from web.config import DEBUG
@@ -12,14 +19,20 @@ from web.models.users import fake_users_db
 
 logger = logging.getLogger(__name__)
 
-router = APIRouter()
+# to get a string like this run:
+# openssl rand -hex 32 (in git bash)
+SECRET_KEY = "c21e47411e5f6aa9ea573ca6efbbc1195320cf976c7163294cc3eb0178f41314"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class User(BaseModel):
@@ -33,10 +46,45 @@ class UserInDB(User):
     hashed_password: str
 
 
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+router = APIRouter()
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
+
+
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
+
+
+def authentificate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 def fake_decode_token(token):
@@ -47,13 +95,22 @@ def fake_decode_token(token):
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -65,6 +122,7 @@ async def get_current_active_user(
     return current_user
 
 
+"""
 @router.post("/auth/token")
 async def login(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
@@ -78,10 +136,36 @@ async def login(
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     return {"access_token": user.username, "token_type": "bearer"}
+"""
 
 
-@router.get("/auth/me")
+@router.post("/auth/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:  # 第一引数がクラスの場合Depends()の引数を省略可能
+    user = authentificate_user(
+        fake_users_db, form_data.username, form_data.password
+    )  # 入力されたユーザー名が含まれる辞書を取得
+    if not user:  # ログインエラー
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/auth/me", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return current_user
+
+
+@router.get("/userauth/me/items/")
+async def read_own_items(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
